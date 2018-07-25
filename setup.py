30d97110a1138442df702376d7318d82f4eed9e1
Harvey Rendell <harvey.rendell@orionhealth.com>
2018-07-25 13:32:23 +1200

    Add setup.py for packaging module source

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='discord-karma',
+    version='0.0.1',
+    description='Karma Bot for Discord',
+    author='Harvey Rendell',
+    author_email='hjrendell@gmail.com',
+    url='https://github.com/Puhapig/discord-karma',
+    packages=['karma'],
+)
